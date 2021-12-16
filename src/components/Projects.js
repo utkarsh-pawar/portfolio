1a7dc35b85ebbecb62f3b550f3c0fa686903d893
utkarsh <utkarshpawar910910@gmail.com>
2021-12-16 17:00:31 +0530

    projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/Projects.module.css";
 import { projectData } from "../data/project-data";
+import { FaExternalLinkAlt, FaCode } from 'react-icons/fa'
 
 const Projects = () => {
   return (
@@ -15,7 +16,22 @@ const Projects = () => {
             <div className={styles['projects-children']}>1</div>
             <div className={styles['projects-children']}>1</div> */}
       {projectData.map((project) => {
-        return <div className={styles["projects-children"]}></div>;
+        return <div className={styles["project"]}>
+          <div className={styles['project-img-div']}>
+
+            <div className={styles['project-img']} style={{ background: `url(${project.img})` }}></div>
+          </div>
+          <div className={styles['projects-desc']}>
+            <h2>{project.title}</h2>
+            <p>{project.desc}</p>
+            <div>
+              <a target="_blank" rel="noreferrer" href={project.github}><FaCode /></a>
+              <a target="_blank" rel="noreferrer" href={project.url}><FaExternalLinkAlt /></a>
+            </div>
+          </div>
+
+
+        </div>;
       })}
     </div>
   );
--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,32 +1,39 @@
 import googleUi from "../assets/google-ui.png";
+import toursApi from '../assets/tours-api.png';
+import moviesApi from '../assets/movies-imdb-clone.png'
+import portfolioImg from '../assets/portfolio.png'
+
 
 export const projectData = [
   {
     id: 1,
     title: "Google UI Clone",
     img:  googleUi ,
-    github: "",
-    url: "",
+    github: "https://github.com/utkarsh-pawar/google-ui-clone",
+    url: "https://google-ui-clone-iota.vercel.app",
   },
   {
     id: 2,
-    title: "Google UI Clone",
-    img: "",
-    github: "",
-    url: "",
+    title: "Tours fetcher",
+    img: toursApi,
+    github: "https://github.com/utkarsh-pawar/tours-react-api",
+    url: "https://tours-react-api.vercel.app",
+    desc:''
   },
   {
     id: 3,
-    title: "Google UI Clone",
-    img: "",
-    github: "",
-    url: "",
+    title: "IMDB clone",
+    img: moviesApi,
+    github: "https://github.com/utkarsh-pawar/movies-react-api",
+    url: "https://movies-react-api.vercel.app",
+    desc:''
   },
   {
     id: 4,
-    title: "Google UI Clone",
-    img: "",
-    github: "",
-    url: "",
+    title: "Personal Portfolio",
+    img: portfolioImg,
+    github: "https://github.com/utkarsh-pawar/portfolio",
+    url: "https://utkarsh-pawar.vercel.app",
+    desc:''
   },
 ];
